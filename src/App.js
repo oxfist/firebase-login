92cb0d5adde9c01801c81b02c6b05cf0ef751542
Andrés Quilodrán <andres.caro.q@gmail.com>
2021-09-25 00:23:39 -0300

    Small index cleanup

--- src/App.js
+++ src/App.js
@@ -37,14 +37,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <button onClick={ signInWithGoogle }>Iniciar sesión con Google</button>
       </header>
     </div>
