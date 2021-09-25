9081402fd5abe04fd1a82b71c92ca6efade3e425
Andrés Quilodrán <andres.caro.q@gmail.com>
2021-09-25 00:22:51 -0300

    Add base Google Auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJHmfUMMEAn87wjxd-MXxrpx5thaMRhhc",
+  authDomain: "fir-login-4641c.firebaseapp.com",
+  projectId: "fir-login-4641c",
+  storageBucket: "fir-login-4641c.appspot.com",
+  messagingSenderId: "744107058336",
+  appId: "1:744107058336:web:2b59054ca0ff8869ed15c5"
+};
+
+const firebase = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+const auth = getAuth(firebase);
+
+const signInWithGoogle = () => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+
+      console.log(token, user);
+    }).catch((error) => {
+      console.error('LOGIN ERROR', error);
+    });
+}
 
 function App() {
   return (
@@ -17,6 +45,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={ signInWithGoogle }>Iniciar sesión con Google</button>
       </header>
     </div>
   );
